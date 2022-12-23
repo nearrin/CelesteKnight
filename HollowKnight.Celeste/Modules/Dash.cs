3dd4b6df7514424499f63b6d83f73fcfe1a061ba
Nearrin Yu <nearrin@outlook.com>
2022-12-22 23:47:08 -0800

    Fix dash sprites.

--- HollowKnight.Celeste/Modules/Dash.cs
+++ HollowKnight.Celeste/Modules/Dash.cs
@@ -4,26 +4,67 @@ namespace Celeste
     {
         private bool dashingUp = false;
         private bool dashingDown = false;
-        private bool dashingHorizontal = false;
+        private bool dashingLeft = false;
+        private bool dashingRight = false;
         public override void SetActive(bool active)
         {
             if (active)
             {
                 On.HeroController.HeroDash += HeroController_HeroDash;
                 ModHooks.DashVectorHook += ModHooks_DashVectorHook;
+                On.HeroController.FinishedDashing += HeroController_FinishedDashing;
             }
             else
             {
                 On.HeroController.HeroDash -= HeroController_HeroDash;
                 ModHooks.DashVectorHook -= ModHooks_DashVectorHook;
+                On.HeroController.FinishedDashing -= HeroController_FinishedDashing;
             }
+        }
+        private void RotateSprite(int direction)
+        {
+            var h = HeroController.instance;
+            var r = h.transform.localRotation.eulerAngles;
+            var a = 0;
+            if (dashingUp)
+            {
+                if (dashingLeft)
+                {
+                    a = -135;
+                }
+                else if (dashingRight)
+                {
+                    a = 135;
+                }
+                else
+                {
+                    a = 180;
+                }
+            }
+            else if (dashingDown)
+            {
+                if (dashingLeft)
+                {
+                    a = -45;
+                }
+                else if (dashingRight)
+                {
+                    a = 45;
+                }
+            }
+            var newR = new Vector3(r.x, r.y, r.z + direction * a);
+            var c = h.gameObject.GetComponent<BoxCollider2D>().bounds.center;
+            h.transform.localRotation = Quaternion.Euler(newR);
+            var newC = h.gameObject.GetComponent<BoxCollider2D>().bounds.center;
+            var p = h.transform.localPosition;
+            var newP = p + c - newC;
+            h.transform.localPosition = newP;
         }
         private void HeroController_HeroDash(On.HeroController.orig_HeroDash orig, HeroController self)
         {
             var this_ = self;
             var this__audioCtrl = ReflectionHelper.GetField<HeroController, HeroAudioController>(this_, "audioCtrl");
             var this__inputHandler = ReflectionHelper.GetField<HeroController, InputHandler>(this_, "inputHandler");
-            var this__audioSource = ReflectionHelper.GetField<HeroController, AudioSource>(this_, "audioSource");
             if (!this_.cState.onGround && !this_.inAcid)
             {
                 ReflectionHelper.SetField(this_, "airDashed", true);
@@ -50,108 +91,38 @@ namespace Celeste
             this_.cState.dashing = true;
             ReflectionHelper.SetField(this_, "dashQueueSteps", 0);
             HeroActions inputActions = this__inputHandler.inputActions;
-            if (Input.instance.upPressed())
+            if (Input.instance.upPressed() || Input.instance.downPressed())
             {
+                this_.dashBurst.transform.localPosition = new Vector3(-0.07f, 3.74f, 0.01f);
+                this_.dashBurst.transform.localEulerAngles = new Vector3(0f, 0f, 90f);
                 this_.dashingDown = true;
-                dashingUp = true;
-                dashingDown = false;
-                if (Input.instance.leftPressed() || Input.instance.rightPressed())
-                {
-                    this_.dashBurst.transform.localPosition = new Vector3(-0.07f, 3.74f, 0.01f);
-                    this_.dashBurst.transform.localEulerAngles = new Vector3(0f, 0f, 90f);
-                    dashingHorizontal = true;
-                }
-                else
-                {
-                    this_.dashBurst.transform.localPosition = new Vector3(-0.07f, 3.74f, 0.01f);
-                    this_.dashBurst.transform.localEulerAngles = new Vector3(0f, 0f, 90f);
-                    dashingHorizontal = false;
-                }
-            }
-            else if (Input.instance.downPressed())
-            {
-                this_.dashingDown = true;
-                dashingUp = false;
-                dashingDown = true;
-                if (Input.instance.leftPressed() || Input.instance.rightPressed())
-                {
-                    this_.dashBurst.transform.localPosition = new Vector3(-0.07f, 3.74f, 0.01f);
-                    this_.dashBurst.transform.localEulerAngles = new Vector3(0f, 0f, 90f);
-                    dashingHorizontal = true;
-                }
-                else
-                {
-                    this_.dashBurst.transform.localPosition = new Vector3(-0.07f, 3.74f, 0.01f);
-                    this_.dashBurst.transform.localEulerAngles = new Vector3(0f, 0f, 90f);
-                    dashingHorizontal = false;
-                }
             }
             else
             {
                 this_.dashBurst.transform.localPosition = new Vector3(4.11f, -0.55f, 0.001f);
                 this_.dashBurst.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
                 this_.dashingDown = false;
-                dashingUp = false;
-                dashingDown = false;
-                dashingHorizontal = true;
             }
+            var s = this_.dashBurst.transform.localScale;
+            s = new Vector3(s.x, 1, s.z);
+            this_.dashBurst.transform.localScale = s;
+            dashingUp = Input.instance.upPressed();
+            dashingDown = Input.instance.downPressed();
+            dashingLeft = Input.instance.leftPressed();
+            dashingRight = Input.instance.rightPressed();
             ReflectionHelper.SetField(this_, "dashCooldownTimer", 0.2f);
-            if (this_.playerData.GetBool("hasShadowDash") && ReflectionHelper.GetField<HeroController, float>(this_, "shadowDashTimer") <= 0f)
-            {
-                ReflectionHelper.SetField(this_, "shadowDashTimer", this_.SHADOW_DASH_COOLDOWN);
-                this_.cState.shadowDashing = true;
-                if (this_.playerData.GetBool("equippedCharm_16"))
-                {
-                    this__audioSource.PlayOneShot(this_.sharpShadowClip, 1f);
-                    this_.sharpShadowPrefab.SetActive(true);
-                }
-                else
-                {
-                    this__audioSource.PlayOneShot(this_.shadowDashClip, 1f);
-                }
-            }
-            if (this_.cState.shadowDashing)
-            {
-                if (this_.dashingDown)
-                {
-                    var this__dashEffect = this_.shadowdashDownBurstPrefab.Spawn(new Vector3(this_.transform.position.x, this_.transform.position.y + 3.5f, this_.transform.position.z + 0.00101f));
-                    this__dashEffect.transform.localEulerAngles = new Vector3(0f, 0f, 90f);
-                    ReflectionHelper.SetField(this_, "dashEffect", this__dashEffect);
-                }
-                else if (this_.transform.localScale.x > 0f)
-                {
-                    var this__dashEffect = this_.shadowdashBurstPrefab.Spawn(new Vector3(this_.transform.position.x + 5.21f, this_.transform.position.y - 0.58f, this_.transform.position.z + 0.00101f));
-                    this__dashEffect.transform.localScale = new Vector3(1.919591f, this__dashEffect.transform.localScale.y, this__dashEffect.transform.localScale.z);
-                    ReflectionHelper.SetField(this_, "dashEffect", this__dashEffect);
-                }
-                else
-                {
-                    var this__dashEffect = this_.shadowdashBurstPrefab.Spawn(new Vector3(this_.transform.position.x - 5.21f, this_.transform.position.y - 0.58f, this_.transform.position.z + 0.00101f));
-                    this__dashEffect.transform.localScale = new Vector3(-1.919591f, this__dashEffect.transform.localScale.y, this__dashEffect.transform.localScale.z);
-                    ReflectionHelper.SetField(this_, "dashEffect", this__dashEffect);
-                }
-                this_.shadowRechargePrefab.SetActive(true);
-                FSMUtility.LocateFSM(this_.shadowRechargePrefab, "Recharge Effect").SendEvent("RESET");
+            this_.dashBurst.SendEvent("PLAY");
 #pragma warning disable 612, 618
-                this_.shadowdashParticlesPrefab.GetComponent<ParticleSystem>().enableEmission = true;
+            this_.dashParticlesPrefab.GetComponent<ParticleSystem>().enableEmission = true;
 #pragma warning restore 612, 618
-                VibrationManager.PlayVibrationClipOneShot(this_.shadowDashVibration, null, false, "");
-                this_.shadowRingPrefab.Spawn(this_.transform.position);
-            }
-            else
-            {
-                this_.dashBurst.SendEvent("PLAY");
-#pragma warning disable 612, 618
-                this_.dashParticlesPrefab.GetComponent<ParticleSystem>().enableEmission = true;
-#pragma warning restore 612, 618
-                VibrationManager.PlayVibrationClipOneShot(this_.dashVibration, null, false, "");
-            }
-            if (this_.cState.onGround && !this_.cState.shadowDashing)
+            VibrationManager.PlayVibrationClipOneShot(this_.dashVibration, null, false, "");
+            if (this_.cState.onGround)
             {
                 var this__dashEffect = this_.backDashPrefab.Spawn(this_.transform.position);
                 this__dashEffect.transform.localScale = new Vector3(this_.transform.localScale.x * -1f, this_.transform.localScale.y, this_.transform.localScale.z);
                 ReflectionHelper.SetField(this_, "dashEffect", this__dashEffect);
             }
+            RotateSprite(1);
         }
         private Vector2 ModHooks_DashVectorHook(Vector2 arg)
         {
@@ -161,7 +132,7 @@ namespace Celeste
             if (dashingUp || dashingDown)
             {
                 var vY = (dashingUp ? 1 : -1) * v;
-                if (dashingHorizontal)
+                if (dashingLeft || dashingRight)
                 {
                     var k = Mathf.Sqrt(2) / 2;
                     return new Vector2(k * vX, k * vY);
@@ -180,6 +151,11 @@ namespace Celeste
                 }
                 return new Vector2(vX, vY);
             }
+        }
+        private void HeroController_FinishedDashing(On.HeroController.orig_FinishedDashing orig, HeroController self)
+        {
+            RotateSprite(-1);
+            orig(self);
         }
     }
 }
